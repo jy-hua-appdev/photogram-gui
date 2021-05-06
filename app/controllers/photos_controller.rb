e51b863b616aa6cdc78295802f3a6762266a7095
jy-hua <jhuang17@chicagobooth.edu>
2021-05-06 21:06:19 +0000

    Delete photo implemented

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,4 +8,26 @@ class PhotosController < ApplicationController
 
   end
 
+  def show
+
+    matching_photos = Photo.where({ :id => params.fetch("path_id") })
+
+    @the_photo = matching_photos.at(0)
+
+    render({ :template => "/photo_templates/show.html.erb" })
+
+  end
+
+  def delete
+
+    matching_photos = Photo.where({ :id => params.fetch("path_id") })
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    redirect_to("/photos")
+
+  end
+
 end
