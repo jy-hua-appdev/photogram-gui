0970ef47c64f68a17261ce168d08886ceec3fae4
jy-hua <jhuang17@chicagobooth.edu>
2021-05-06 20:36:57 +0000

    Photos index looking good

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,11 @@
 class PhotosController < ApplicationController
 
   def index
+
+    @list_of_photos = Photo.all.order({ :created_at => :desc })
+
     render({ :template => "/photo_templates/index.html.erb" })
+
   end
 
 end
